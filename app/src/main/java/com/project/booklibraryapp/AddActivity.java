5ad9c6cee8d12044c984b25b72795a74e24833a5
Peter Rai <78395050+peterraitamang@users.noreply.github.com>
2023-01-22 20:43:40 +0545

    Initial Commit

---
+++ app/src/main/java/com/project/booklibraryapp/AddActivity.java
@@ -0,0 +1,35 @@
+package com.project.booklibraryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddActivity extends AppCompatActivity {
+
+    EditText title_input, author_input, pages_input;
+    Button add_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        title_input = findViewById(R.id.et_book_title2);
+        author_input = findViewById(R.id.et_book_author2);
+        pages_input= findViewById(R.id.et_noOfPages2);
+        add_button = findViewById(R.id.btn_update);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(AddActivity.this);
+                myDB.addBook(title_input.getText().toString().trim(),
+                        author_input.getText().toString().trim(),
+                        Integer.valueOf(pages_input.getText().toString().trim()));
+            }
+        });
+    }
+}
